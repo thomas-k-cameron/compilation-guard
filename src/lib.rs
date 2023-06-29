85c38ff585da5de8247526063bf2758ef547e56f
Thomas Cameron <thomas.k.cameron.1218@gmail.com>
2023-06-29 12:45:13 +0000

    compilation guard

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+use proc_macro::{self, TokenStream};
+
+#[proc_macro_derive(CompilationGuard)]
+pub fn derive(input: TokenStream) -> TokenStream {
+    panic_logic(input);
+    unreachable!();
+}
+
+#[proc_macro_attribute]
+pub fn compilation_guard(input: TokenStream, _: TokenStream) -> TokenStream {
+    panic_logic(input);
+    unreachable!()
+}
+
+fn panic_logic(input: TokenStream) {
+    let mut iter = input.into_iter();
+    match (iter.next(), iter.next()) {
+        (Some(proc_macro::TokenTree::Literal(literal)), None) => {
+            let s = format!("Compilation guard was triggered!\n{}", literal.to_string());
+            panic!("{s}");
+        }
+        _ => panic!("This macro is expected to receive a `literal` as an argument.")
+    }
+}
