6ca6b25d10b06590852053849102fec0791ea9ac
Thomas Cameron <thomas.k.cameron.1218@gmail.com>
2023-07-03 00:42:20 +0000

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,33 @@
 use proc_macro::{self, TokenStream};
 
+/// Forces compilation to fail.
+/// 
+/// e.g. 
+/// ```rust
+/// #![allow(unused_imports, dead_code)]
+/// 
+/// use compilation_guard::compilation_guard;
+/// #[cfg_attr(all(unstable, feature = "enable-macro"), derive(VeryExpensiveMacro))]
+/// #[cfg_attr(all(not(unstable), feature = "enable-macro"), compilation_guard("lol"))]
+/// struct A;
+/// 
+/// fn main() {
+/// 
+/// }
+/// ```
+/// 
+/// If you don't enable the unstable flag, you get this.
+/// ```
+/// error: custom attribute panicked
+///  --> src/main.rs:5:58
+///   |
+/// 5 | #[cfg_attr(all(not(unstable), feature = "enable-macro"), compilation_guard("lol"))]
+///   |                                                          ^^^^^^^^^^^^^^^^^^^^^^^^
+///   |
+///   = help: message: Compilation guard was triggered!
+///           lol
+/// ```
+/// 
 #[proc_macro_attribute]
 pub fn compilation_guard(input: TokenStream, _: TokenStream) -> TokenStream {
     panic_logic(input);
